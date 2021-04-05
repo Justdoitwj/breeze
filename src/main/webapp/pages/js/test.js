bd065249821a1f789b6b097e5bfc9e3c4a307b82
wangjie <13227866253@163.com>
2021-04-05 23:01:30 +0800

    提交前端学习代码

---
+++ src/main/webapp/pages/js/test.js
@@ -0,0 +1,46 @@
+function NumberToChinese(num){
+    var unitPos = 0;
+    var strIns = '', chnStr = '';
+    var needZero = false;
+
+    if(num === 0){
+        return chnNumChar[0];
+    }
+
+    while(num > 0){
+        var section = num % 10000;
+        if(needZero){
+            chnStr = chnNumChar[0] + chnStr;
+        }
+        strIns = SectionToChinese(section);
+        strIns += (section !== 0) ? chnUnitSection[unitPos] : chnUnitSection[0];
+        chnStr = strIns + chnStr;
+        needZero = (section < 1000) && (section > 0);
+        num = Math.floor(num / 10000);
+        unitPos++;
+    }
+
+    return chnStr;
+}
+function SectionToChinese(section){
+    var strIns = '', chnStr = '';
+    var unitPos = 0;
+    var zero = true;
+    while(section > 0){
+        var v = section % 10;
+        if(v === 0){
+            if(!zero){
+                zero = true;
+                chnStr = chnNumChar[v] + chnStr;
+            }
+        }else{
+            zero = false;
+            strIns = chnNumChar[v];
+            strIns += chnUnitChar[unitPos];
+            chnStr = strIns + chnStr;
+        }
+        unitPos++;
+        section = Math.floor(section / 10);
+    }
+    return chnStr;
+}
